09859668370b2a1a1977bf579a71da0e3f79c310
Muhammad Qasim <malikqasim20051@gmail.com>
2024-06-23 23:59:20 +0500

    Dynamic Ice Blast Section

--- Dari_Mooch/views.py
+++ Dari_Mooch/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-
 def homePage(request):
-    return render(request, 'index.html')
+    Ice_Blast_Section_Data= {
+        "IceBlast":[{"id":1,"title":"Ice Blast Bundle","price":1399.00,"description":"K","image":"static/images/1.webp"},{"id":2,"title":"Ice Blast Face Wash","price":799.00,"description":"S","image":"static/images/2.webp"},{"id":3,"title":"Ice Blast Body Wash","price":699.00,"description":"great","image":"static/images/3.webp"}]
+    }
+    return render(request, 'index.html', Ice_Blast_Section_Data)
+
+
 
 def aboutPage(request):
     return render(request, 'about.html')
 
 def loginPage(request):
     return render(request, 'login.html')
-
