ee8985b00eef3626485d962c04526d4ab5793015
Muhammad Qasim <malikqasim20051@gmail.com>
2024-06-25 22:12:53 +0500

    Ice Blast Section Completed

--- Dari_Mooch_Products/admin.py
+++ Dari_Mooch_Products/admin.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 from .models import Products
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title','price']
+    list_display = ['title','price','image']
 
 admin.site.register(Products,ProductAdmin)
--- Dari_Mooch_Products/models.py
+++ Dari_Mooch_Products/models.py
@@ -6,3 +6,4 @@ class Products(models.Model):
     description = models.TextField(max_length=60)
     category = models.CharField(max_length=60)
     price = models.IntegerField()
+    image = models.FileField(max_length=200, upload_to="products/",null=True)
